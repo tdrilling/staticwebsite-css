38824c9cec85948fd830dc3b69bf3bd2a7416f0b
Thomas Drilling <37440018+tdrilling@users.noreply.github.com>
2021-11-25 12:58:35 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$( "#mehrErfahren" ).click(function() {
+  //alert("Mehr erfahren... angeklickt.");
+  $("#mehrText").text("Der Roboter, den du auf dem Bild sehen kannst, ist ziemlich cool. Auf lerneProgrammieren.de kannst du ihn per Code steuern und damit auf eine einfache Art und Weise das Programmieren mit Javascript lernen.");
+  $("#mehrErfahren").hide();
+});
